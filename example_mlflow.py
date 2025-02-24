0cd0e39ee00ccc4814e2ca6e14fcd14b6de1e608
atolopko-czi <atolopko@chanzuckerberg.com>
2025-02-24 21:38:57 +0000

    tweak

--- example_mlflow.py
+++ example_mlflow.py
@@ -5,33 +5,35 @@ from czibench.datasets.utils import init_dataset
 import scanpy as sc
 import argparse
 
-parser = argparse.ArgumentParser(description="Run MLflow model on dataset")
-parser.add_argument('--model-mode', choices=['local', 'remote'], default='local', help='Mode to run the model (default: local)')
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(description="Run MLflow model on dataset")
+    parser.add_argument('--model-mode', choices=['local', 'remote'], default='local', help='Mode to run the model (default: local)')
+    args = parser.parse_args()
 
-if args.model_mode == 'local':
-    dataset = init_dataset("example-local", config_path="custom.yaml")
-    runner = MLflowModelRunner(model_resource_url="model-serving/runtimes/mlflow/scvi/runtime")
-else:
-    dataset = init_dataset("example-remote", config_path="custom.yaml")
-    runner = MLflowModelRunner(model_endpoint="https://czi-virtual-cells-dev-databricks-workspace.cloud.databricks.com/serving-endpoints/scvi4/invocations")
+    if args.model_mode == 'local':
+        dataset = init_dataset("example-local", config_path="custom.yaml")
+        runner = MLflowModelRunner(model_resource_url="model-serving/runtimes/mlflow/scvi/runtime")
+    else:
+        dataset = init_dataset("example-remote", config_path="custom.yaml")
+        runner = MLflowModelRunner(model_endpoint="https://czi-virtual-cells-dev-databricks-workspace.cloud.databricks.com/serving-endpoints/scvi4/invocations")
 
-dataset = runner.run(dataset)
-dataset.load_data()
-print(dataset.get_output(DataType.EMBEDDING))
+    dataset = runner.run(dataset)
+    dataset.load_data()
+    print(dataset.get_output(DataType.EMBEDDING))
 
-task = ClusteringTask(label_key="cell_type")
-dataset, clustering_results = task.run(dataset)
-print(clustering_results)
+    task = ClusteringTask(label_key="cell_type")
+    dataset, clustering_results = task.run(dataset)
+    print(clustering_results)
 
-task = EmbeddingTask(label_key="cell_type")
-dataset, embedding_results = task.run(dataset)
-print(embedding_results)
+    task = EmbeddingTask(label_key="cell_type")
+    dataset, embedding_results = task.run(dataset)
+    print(embedding_results)
 
-sc.tl.umap(dataset.adata)
-sc.pl.umap(dataset.adata, color="cell_type")
+    sc.tl.umap(dataset.adata)
+    sc.pl.umap(dataset.adata, color="cell_type")
 
-dataset.unload_data()
-dataset.serialize("data.dill")
+    dataset.unload_data()
+    dataset.serialize("data.dill")
 
-
+if __name__ == "__main__":
+    main()
