8513ada8e9bfbe0b89de30dc2911fc7f61080802
atolopko <atolopko@chanzuckerberg.com>
2025-02-28 13:00:42 -0500

    fix mlflow local param passing

--- src/czibench/runners/mlflow_runner.py
+++ src/czibench/runners/mlflow_runner.py
@@ -15,23 +15,28 @@ class MLflowModelRunner(ModelRunnerBase):
 
     def _run_local(self, dataset: BaseDataset) -> BaseDataset:
         with tempfile.NamedTemporaryFile(mode="w") as output:
-            print(f"Calling MLflow model process (localhost): {self.model_endpoint}")
-            prediction = mlflow.models.predict(
-                model_uri=self.model_resource_url, 
-                input_data=dataset.local_path,
-                # FIXME: Figure out how to pass additional params to model, if possible
-                # params={"organism": dataset.get_input(DataType.ORGANISM)}, 
-                output_path=output.name,
-                env_manager="uv",
-                # FIXME: this is not working; as is, it uses /tmp/
-                extra_envs={"MLFLOW_ENV_ROOT": "mlflow-envs"}
-            )
-            output.flush()
-            with open(output.name) as f:
-                prediction_json = json.load(f)
-                prediction = np.array(prediction_json["predictions"])
-                dataset.set_output(DataType.EMBEDDING, prediction)
-            return dataset
+            with tempfile.NamedTemporaryFile(mode="w") as input:
+                input_json = json.dumps({"inputs": [[dataset.source_path]],
+                                         "params": {"organism": str(dataset.get_input(DataType.ORGANISM))}})
+                input.write(input_json)
+                input.flush()
+                print(f"Calling MLflow model process (localhost): {self.model_endpoint}")
+                prediction = mlflow.models.predict(
+                    model_uri=self.model_resource_url, 
+                    # NOTE: You cannot pass params if you use `input_data`; must use `input_path` with a file containing JSON with `input_data` and `params` objects.
+                    # input_data=dataset.local_path,
+                    input_path=input.name,
+                    output_path=output.name,
+                    env_manager="uv",
+                    # FIXME: this is not working; as is, it uses /tmp/
+                    extra_envs={"MLFLOW_ENV_ROOT": "mlflow-envs"}
+                )
+                output.flush()
+                with open(output.name) as f:
+                    prediction_json = json.load(f)
+                    prediction = np.array(prediction_json["predictions"])
+                    dataset.set_output(DataType.EMBEDDING, prediction)
+                return dataset
 
     def _run_remote(self, dataset: BaseDataset) -> BaseDataset:
         token = os.environ.get("DATABRICKS_TOKEN")
