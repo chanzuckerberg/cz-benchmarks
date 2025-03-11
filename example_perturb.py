7e59076e8dc73899ab81c535b5b36ed239a51f8e
atarashansky <atarashansky@chanzuckerberg.com>
2025-03-11 10:50:09 -0700

    remove unnecessary loop

--- example_perturb.py
+++ example_perturb.py
@@ -7,8 +7,7 @@ from czbenchmarks.tasks import (
 if __name__ == "__main__":
     dataset = load_dataset("adamson_perturb")
 
-    for model_name in ["SCGENEPT"]:
-        dataset = run_inference(model_name, dataset, gene_pert="TMED2+ctrl")
+    dataset = run_inference("SCGENEPT", dataset, gene_pert="TMED2+ctrl")
 
     task = PerturbationTask()
     perturbation_results = task.run(dataset)
