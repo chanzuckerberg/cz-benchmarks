971876259e7a1af756456ccbf5a67fb13b0f6d0f
Prathap Sridharan <psridharan@chanzuckerberg.com>
2025-03-12 21:37:29 +0000

    Add implementation of local batch transform

--- model-serving/runtimes/sagemaker/scvi/local/deploy.py
+++ model-serving/runtimes/sagemaker/scvi/local/deploy.py
@@ -29,17 +29,23 @@ def serve_model_locally():
         entry_point="inference.py",
         source_dir="code/",
         sagemaker_session=sagemaker_session,
-        name=MODEL_NAME,
-    )
+        name=MODEL_NAME)
     logger.info(f"Local model '{MODEL_NAME}' has been created.")
 
     # Deploy the model locally.
     # Use instance_type="local" (or "local_gpu" if GPU support is available).
-    predictor = pytorch_model.deploy(initial_instance_count=1, instance_type="local_gpu")
+    env_config = {
+        "TS_MAX_RESPONSE_SIZE": "2000000000",
+        "TS_MAX_REQUEST_SIZE": "2000000000"
+    }
+    predictor = pytorch_model.deploy(
+        initial_instance_count=1,
+        instance_type="local_gpu",
+        env=env_config
+        )
     logger.info("Model is deployed locally and ready to accept predictions.")
 
     return predictor
-
 
 if __name__ == "__main__":
     serve_model_locally()
