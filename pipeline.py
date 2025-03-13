4dce9aed88e6c9c8944f31a316a3f286dfe6521b
atarashansky <atarashansky@chanzuckerberg.com>
2025-03-13 17:33:59 +0000

    update with baselines

--- pipeline.py
+++ pipeline.py
@@ -20,21 +20,29 @@ if __name__ == "__main__":
             
         task = ClusteringTask(label_key="cell_type")
         clustering_results = task.run(dataset)
+        clustering_results_baseline = task.run_baseline(dataset)
         
         task = EmbeddingTask(label_key="cell_type")
         embedding_results = task.run(dataset)
+        embedding_results_baseline = task.run_baseline(dataset)
         
         task = MetadataLabelPredictionTask(label_key="cell_type")
         metadata_results_cell_type = task.run(dataset)
+        metadata_results_cell_type_baseline = task.run_baseline(dataset)
         
         task = MetadataLabelPredictionTask(label_key="sex")
         metadata_results_sex = task.run(dataset)  
-            
+        metadata_results_sex_baseline = task.run_baseline(dataset)
+        
         all_results[dataset_name] = {
             "clustering_results": clustering_results,
             "embedding_results": embedding_results,
             "metadata_results_cell_type": metadata_results_cell_type,
-            "metadata_results_sex": metadata_results_sex
+            "metadata_results_sex": metadata_results_sex,
+            "clustering_results_baseline": clustering_results_baseline,
+            "embedding_results_baseline": embedding_results_baseline,
+            "metadata_results_cell_type_baseline": metadata_results_cell_type_baseline,
+            "metadata_results_sex_baseline": metadata_results_sex_baseline
         }
         
         pickle.dump(all_results, open("all_results.pkl", "wb"))
