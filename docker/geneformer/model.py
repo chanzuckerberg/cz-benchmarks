95383059dd5d27cdd3108352cf8f68cc9be012c4
atarashansky <atarashansky@chanzuckerberg.com>
2025-03-17 14:13:33 -0700

    lint

--- docker/geneformer/model.py
+++ docker/geneformer/model.py
@@ -41,7 +41,7 @@ class Geneformer(GeneformerValidator, BaseModelImplementation):
         sync_s3_to_local(bucket, key, self.model_weights_dir)
 
         model_weights_dir_parent = Path(self.model_weights_dir).parent
-        
+
         vocabs_bucket = config.geneformer_vocabs_uri.split("/")[2]
         prefix = "/".join(config.geneformer_vocabs_uri.split("/")[3:])
         sync_s3_to_local(
@@ -71,9 +71,15 @@ class Geneformer(GeneformerValidator, BaseModelImplementation):
         tk = TranscriptomeTokenizer(
             custom_attr_name_dict={"cell_idx": "cell_idx"},
             nproc=4,
-            gene_median_file=str(Path(f"{model_weights_dir_parent}/{token_config.gene_median_file}")),
-            token_dictionary_file=str(Path(f"{model_weights_dir_parent}/{token_config.token_dictionary_file}")),
-            gene_mapping_file=str(Path(f"{model_weights_dir_parent}/{token_config.ensembl_mapping_file}")),
+            gene_median_file=str(
+                Path(f"{model_weights_dir_parent}/{token_config.gene_median_file}")
+            ),
+            token_dictionary_file=str(
+                Path(f"{model_weights_dir_parent}/{token_config.token_dictionary_file}")
+            ),
+            gene_mapping_file=str(
+                Path(f"{model_weights_dir_parent}/{token_config.ensembl_mapping_file}")
+            ),
             special_token=(seq_len != 2048),
             model_input_size=seq_len,
         )
@@ -92,7 +98,9 @@ class Geneformer(GeneformerValidator, BaseModelImplementation):
             emb_mode="cell",
             forward_batch_size=32,
             nproc=4,
-            token_dictionary_file=str(Path(f"{model_weights_dir_parent}/{token_config.token_dictionary_file}")),
+            token_dictionary_file=str(
+                Path(f"{model_weights_dir_parent}/{token_config.token_dictionary_file}")
+            ),
             max_ncells=None,
             emb_label=["cell_idx"],
         )
