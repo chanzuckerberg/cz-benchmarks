8c9c704da55275131f5c1d7da4f8e1f019098d43
Michelle Gill <mgill@nvidia.com>
2025-03-26 07:50:59 -0700

    Usage

--- docker_scripts/run_docker.sh
+++ docker_scripts/run_docker.sh
@@ -208,7 +208,7 @@ RED="\033[31m"
 # Check if script is run from correct directory
 if [ ! "$(ls | grep -c docker_scripts)" -eq 1 ]; then
     echo ""
-    echo -e "${RED}Run this script from root directory. Usage: bash ./docker_scripts/run_docker_interactive.sh${RESET}"
+    echo -e "${RED}Run this script from root directory. Usage: bash ./docker_scripts/run_docker_interactive.sh -m MODEL_NAME${RESET}"
     echo ""
     print_usage
     exit 1
