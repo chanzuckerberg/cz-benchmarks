98958d4c3d90fbbafc8e6fcf130fcecec6c168b5
Michelle Gill <mgill@nvidia.com>
2025-03-27 09:52:01 -0700

    Whitespace

--- src/czbenchmarks/utils.py
+++ src/czbenchmarks/utils.py
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 def initialize_hydra(config_path="../../conf"):
     """
     Initialize Hydra configuration system.
-    
+
     Args:
         config_path: Path to the configuration directory
     """
