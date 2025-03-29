c510047791565c59b47ba4db7aa6af90d0c82350
Michelle Gill <mgill@nvidia.com>
2025-03-28 17:37:57 -0700

    Fix test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,5 @@
-import os
-import hydra
 from omegaconf import OmegaConf
-from czbenchmarks.utils import initialize_hydra, import_class_from_config
+from czbenchmarks.utils import import_class_from_config
 
 
 # Sample test class for import testing
@@ -9,24 +7,6 @@ class TestClass:
     def __init__(self, param1: str, param2: int):
         self.param1 = param1
         self.param2 = param2
-
-
-def test_initialize_hydra():
-    """Test hydra initialization with default and custom config paths."""
-    # Test with default config path
-    initialize_hydra()
-    assert hydra.core.global_hydra.GlobalHydra.instance().is_initialized()
-
-    # Clear hydra
-    hydra.core.global_hydra.GlobalHydra.instance().clear()
-
-    # Test with custom config path
-    custom_path = os.path.join(os.path.dirname(__file__), "conf")
-    initialize_hydra(custom_path)
-    assert hydra.core.global_hydra.GlobalHydra.instance().is_initialized()
-
-    # Clean up
-    hydra.core.global_hydra.GlobalHydra.instance().clear()
 
 
 def test_import_class_from_config(tmp_path):
