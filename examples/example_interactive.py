5e2a86cba26665fe5132cb7bc2ec130e086c1a5f
Michelle Gill <mgill@nvidia.com>
2025-03-28 17:28:39 -0700

    Update example to use import from file

--- examples/example_interactive.py
+++ examples/example_interactive.py
@@ -1,24 +1,23 @@
 import sys
 import os
-from czbenchmarks.datasets.utils import load_dataset
+import logging
 
+from czbenchmarks.utils import import_class_from_config
+from czbenchmarks.datasets.utils import load_dataset
 from czbenchmarks.tasks import (
     ClusteringTask,
     EmbeddingTask,
     MetadataLabelPredictionTask,
 )
 
-if os.path.exists("/app/model.py"):
-    sys.path.insert(0, "/app")
-    from model import SCVI as BenchmarkModel
-else:
-    raise ValueError(
-        "Model not found in /app/model.py. "
-        "This example should be run in a Docker container."
-    )
+logger = logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+
+# Import the model class based on _target_ field in the config.yaml file
+APP_PATH = os.environ.get("APP_PATH", "/app")
+BenchmarkModel = import_class_from_config(os.path.join(APP_PATH, "config.yaml"))
 
 if __name__ == "__main__":
-    dataset_list = ["tsv2_heart", "tsv2_large_intestine"]
+    dataset_list = ["tsv2_bladder", "tsv2_large_intestine"]
     datasets = [
         load_dataset(dataset_name=dataset_name) for dataset_name in dataset_list
     ]
