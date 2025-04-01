df6adb1b2e37bcabc4ad2f4826497ed9e461c6f6
Omar Valenzuela <ovalenzuela@chanzuckerberg.com>
2025-04-01 00:05:38 +0000

    cleanup
    Signed-off-by: Omar Valenzuela <ovalenzuela@chanzuckerberg.com>

--- example.py
+++ example.py
@@ -18,15 +18,15 @@ if __name__ == "__main__":
         task = ClusteringTask(label_key="cell_type")
         clustering_results = task.run(dataset)
 
-    # task = EmbeddingTask(label_key="cell_type")
-    # embedding_results = task.run(dataset)
+    task = EmbeddingTask(label_key="cell_type")
+    embedding_results = task.run(dataset)
 
-    # task = MetadataLabelPredictionTask(label_key="cell_type")
-    # prediction_results = task.run(dataset)
+    task = MetadataLabelPredictionTask(label_key="cell_type")
+    prediction_results = task.run(dataset)
 
     print("Clustering results:")
     print(clustering_results)
-    # print("Embedding results:")
-    # print(embedding_results)
-    # print("Prediction results:")
-    # print(prediction_results)
+    print("Embedding results:")
+    print(embedding_results)
+    print("Prediction results:")
+    print(prediction_results)
