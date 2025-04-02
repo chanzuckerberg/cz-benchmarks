45c0a2b044667ef742fe07aa85724e84426bf296
Jonathan Schoonhoven <jschoonhoven@chanzuckerberg.com>
2025-04-02 10:19:45 -0700

    fix example

--- example.py
+++ example.py
@@ -13,20 +13,20 @@ if __name__ == "__main__":
     dataset = load_dataset("tsv2_bladder")
 
     for model_name in ["SCVI", "SCGPT"]:
-        dataset = run_inference(model_name, dataset)
+        run_inference(model_name, dataset)
 
-        task = ClusteringTask(label_key="cell_type")
-        clustering_results = task.run(dataset)
+    task = ClusteringTask(label_key="cell_type")
+    clustering_results = task.run(dataset)
 
-        task = EmbeddingTask(label_key="cell_type")
-        embedding_results = task.run(dataset)
+    task = EmbeddingTask(label_key="cell_type")
+    embedding_results = task.run(dataset)
 
-        task = MetadataLabelPredictionTask(label_key="cell_type")
-        prediction_results = task.run(dataset)
+    task = MetadataLabelPredictionTask(label_key="cell_type")
+    prediction_results = task.run(dataset)
 
-        print("Clustering results:")
-        print(clustering_results)
-        print("Embedding results:")
-        print(embedding_results)
-        print("Prediction results:")
-        print(prediction_results)
+    print("Clustering results:")
+    print(clustering_results)
+    print("Embedding results:")
+    print(embedding_results)
+    print("Prediction results:")
+    print(prediction_results)
