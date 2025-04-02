eb0b7a0fd649d76f6cc02652304740c956a1638b
Jonathan Schoonhoven <jschoonhoven@chanzuckerberg.com>
2025-04-02 10:19:32 -0700

    write results to file

--- src/czbenchmarks/cli.py
+++ src/czbenchmarks/cli.py
@@ -1,15 +1,27 @@
 import logging
+import json
+import os
 import sys
+import tomli
+from pathlib import Path
+from importlib.metadata import version, PackageNotFoundError
 from typing_extensions import TypedDict, NotRequired
+import yaml
+from datetime import datetime
 from czbenchmarks.datasets import utils
 from czbenchmarks.datasets.base import BaseDataset
-from czbenchmarks.runner import ContainerRunner
+from czbenchmarks.metrics.types import MetricResult
+from czbenchmarks.models.types import ModelType
+from czbenchmarks import runner
 from czbenchmarks.tasks.clustering import ClusteringTask
 from czbenchmarks.tasks.embedding import EmbeddingTask
 from czbenchmarks.tasks.label_prediction import MetadataLabelPredictionTask
 from czbenchmarks.tasks.integration import BatchIntegrationTask
 
 log = logging.getLogger(__name__)
+
+VALID_OUTPUT_FORMATS = ["json", "yaml"]
+DEFAULT_OUTPUT_FORMAT = "json"
 
 
 class ClusteringTaskArgs(TypedDict):
@@ -32,6 +44,30 @@ class IntegrationTaskArgs(TypedDict):
     batch_key: NotRequired[str]
 
 
+class TaskResult(TypedDict):
+    results: list[dict[ModelType, list[MetricResult]]]
+    args: (
+        ClusteringTaskArgs
+        | EmbeddingTaskArgs
+        | PredictionTaskArgs
+        | IntegrationTaskArgs
+    )
+
+
+class DatasetResult(TypedDict):
+    dataset_name: str
+    clustering_task: NotRequired[TaskResult]
+    embedding_task: NotRequired[TaskResult]
+    prediction_task: NotRequired[TaskResult]
+    integration_task: NotRequired[TaskResult]
+
+
+class RunResult(TypedDict):
+    results: list[DatasetResult]
+    czbenchmarks_version: str
+    args: str
+
+
 def run(
     *,
     model_names: list[str],
@@ -40,61 +76,131 @@ def run(
     embedding_task_args: EmbeddingTaskArgs | None = None,
     prediction_task_args: PredictionTaskArgs | None = None,
     integration_task_args: IntegrationTaskArgs | None = None,
-) -> None:
+) -> list[DatasetResult]:
     """
     Run a set of tasks for a set of models on a set of datasets.
     """
+    results: list[DatasetResult] = []
 
-    # Load datasets
-    datasets: dict[str, BaseDataset] = {}
     for dataset_name in dataset_names:
+        result = DatasetResult(dataset_name=dataset_name)
+
         log.info(f"Loading dataset {dataset_name}")
-        datasets[dataset_name] = utils.load_dataset(dataset_name)
+        dataset = utils.load_dataset(dataset_name)
 
-    # Load each model
-    for model_name in model_names:
-        log.info(f"Running model {model_name}")
-        runner = ContainerRunner(model_name, gpu=True)
+        # Run inference against this dataset for each model
+        for model_name in model_names:
+            log.info(f"Running {model_name} inference on dataset {dataset_name}")
+            runner.run_inference(model_name, dataset, gpu=True)
 
-        for dataset_name, dataset in datasets.items():
-            log.info(f"Running model {model_name} on dataset {dataset_name}")
-            embeddings = runner.run(dataset)
+        if clustering_task_args:
+            result["clustering_task"] = run_task(
+                ClusteringTask, dataset, clustering_task_args
+            )
+        if embedding_task_args:
+            result["embedding_task"] = run_task(
+                EmbeddingTask, dataset, embedding_task_args
+            )
+        if prediction_task_args:
+            result["prediction_task"] = run_task(
+                MetadataLabelPredictionTask, dataset, prediction_task_args
+            )
+        if integration_task_args:
+            result["integration_task"] = run_task(
+                BatchIntegrationTask, dataset, integration_task_args
+            )
+        results.append(result)
 
-            if clustering_task_args:
-                log.info("Running clustering task with args: %s", clustering_task_args)
-                clustering_task = ClusteringTask(**clustering_task_args)
-                clustering_results = clustering_task.run(embeddings)
-                log.info(
-                    f"Clustering results for {model_name} on {dataset_name}:\n\n{clustering_results}\n"
-                )
+    return results
 
-            if embedding_task_args:
-                log.info("Running embedding task with args: %s", embedding_task_args)
-                embedding_task = EmbeddingTask(**embedding_task_args)
-                embedding_results = embedding_task.run(embeddings)
-                log.info(
-                    f"Embedding results for {model_name} on {dataset_name}:\n\n{embedding_results}\n"
-                )
 
-            if prediction_task_args:
-                log.info("Running prediction task with args: %s", prediction_task_args)
-                prediction_task = MetadataLabelPredictionTask(**prediction_task_args)
-                prediction_results = prediction_task.run(embeddings)
-                log.info(
-                    f"Prediction results for {model_name} on {dataset_name}:\n\n{prediction_results}\n"
-                )
+def run_task(
+    TaskCls: type[ClusteringTask]
+    | type[EmbeddingTask]
+    | type[MetadataLabelPredictionTask]
+    | type[BatchIntegrationTask],
+    embeddings: list[BaseDataset],
+    task_args: ClusteringTaskArgs
+    | EmbeddingTaskArgs
+    | PredictionTaskArgs
+    | IntegrationTaskArgs,
+) -> TaskResult:
+    """
+    Run a task and return the results.
+    """
+    log.info("Running %s with args: %s", TaskCls.__name__, task_args)
+    task = TaskCls(**task_args)
+    results = task.run(embeddings)
 
-            if integration_task_args:
-                log.info(
-                    "Running integration task with args: %s", integration_task_args
-                )
-                integration_task = BatchIntegrationTask(**integration_task_args)
-                integration_results = integration_task.run(embeddings)
-                log.info(
-                    f"Integration results for {model_name} on {dataset_name}:\n\n{integration_results}\n"
-                )
+    if isinstance(results, dict):
+        results = [results]  # Ensure results are always a list
 
-    log.info("success")
+    log.info(
+        "%s results for %s on %s:\n\n%s\n",
+        TaskCls.__name__,
+        task_args["model_name"],
+        task_args["dataset_name"],
+        results,
+    )
+    return TaskResult(results=results, args=task_args)
+
+
+def write_results(
+    results: RunResult,
+    *,
+    output_format: str = DEFAULT_OUTPUT_FORMAT,
+    output_file: str | None = None,  # Writes to stdout if None
+) -> None:
+    # Get the intended format/extension
+    if output_file and output_file.endswith(".json"):
+        output_format = "json"
+    elif output_file and (
+        output_file.endswith(".yaml") or output_file.endswith(".yml")
+    ):
+        output_format = "yaml"
+    elif output_format not in VALID_OUTPUT_FORMATS:
+        raise ValueError(f"Invalid output format: {output_format}")
+
+    # Dump the results to a string
+    result_str = ""
+    if output_format == "json":
+        result_str = json.dumps(results, indent=2)
+    else:
+        result_str = yaml.dump(results)
+
+    # Write to stdout if not otherwise specified
+    if not output_file:
+        sys.stdout.write(result_str)
+        return
+
+    # Generate a unique filename if we were passed a directory
+    if os.path.isdir(output_file) or output_file.endswith("/"):
+        current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+        output_file = os.path.join(
+            output_file, f"czbenchmarks_results_{current_time}.{output_format}"
+        )
+
+    # Write the results to the specified file
+    with open(output_file, "w") as f:
+        f.write(result_str)
+
+
+def get_version() -> str:
+    """
+    Get the current library version.
+    """
+    try:
+        return version("czbenchmarks")
+    except PackageNotFoundError:
+        log.warning(
+            "Package `czbenchmarks` is not installed: fetching version info from pyproject.toml"
+        )
+
+    # In development this lib might not be installed as a package so try loading from pyproject.toml
+    pyproject_path = Path(__file__).parent.parent.parent / "pyproject.toml"
+    with open(pyproject_path, "rb") as f:
+        pyproject = tomli.load(f)
+    return pyproject["project"]["version"]
 
 
 if __name__ == "__main__":
@@ -105,6 +211,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="czbenchmark: A command-line utility for single-cell benchmark tasks."
     )
+    parser.add_argument(
+        "--version",
+        help="Show version number and exit",
+        action="version",
+        version=f"%(prog)s {get_version()}",
+    )
+
     subparsers = parser.add_subparsers(dest="action", required=True)
 
     # Run parser
@@ -123,6 +236,18 @@ if __name__ == "__main__":
         choices=utils.list_available_datasets(),
         required=True,
         help="One or more dataset names (from datasets.yaml).",
+    )
+    run_parser.add_argument(
+        "--output-format",
+        "-fmt",
+        choices=VALID_OUTPUT_FORMATS,
+        default="yaml",
+        help="Output format for results (ignored if --output-file specifies a valid file extension)",
+    )
+    run_parser.add_argument(
+        "--output-file",
+        "-o",
+        help="Path to file or directory to save results (default is stdout)",
     )
 
     # Run parser clustering task
@@ -145,17 +270,17 @@ if __name__ == "__main__":
     run_parser.add_argument(
         "--prediction-task-n-folds",
         type=int,
-        help="Number of cross-validation folds",
+        help="Number of cross-validation folds (optional)",
     )
     run_parser.add_argument(
         "--prediction-task-seed",
         type=int,
-        help="Random seed for reproducibility",
+        help="Random seed for reproducibility (optional)",
     )
     run_parser.add_argument(
         "--prediction-task-min-class-size",
         type=int,
-        help="Minimum samples required per class",
+        help="Minimum samples required per class (optional)",
     )
 
     # Run parser integration task
@@ -218,11 +343,26 @@ if __name__ == "__main__":
                 **{k.removeprefix(pre): v for k, v in args.items() if k.startswith(pre)}
             )
 
-        run(
+        # Run the tasks
+        results = run(
             model_names=args["models"],
             dataset_names=args["datasets"],
             clustering_task_args=clustering_task_args,
             embedding_task_args=embedding_task_args,
             prediction_task_args=prediction_task_args,
             integration_task_args=integration_task_args,
+        )
+
+        # Append some extra metadata before writing the results
+        run_result = RunResult(
+            results=results,
+            czbenchmarks_version=get_version(),
+            args=" ".join(sys.argv),
+        )
+
+        # Write the results to the specified output
+        write_results(
+            run_result,
+            output_format=args["output_format"],
+            output_file=args.get("output_file"),
         )
