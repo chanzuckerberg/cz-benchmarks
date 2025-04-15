73082cd4be6ba7aa1e056a8e3c538ff7c254bc03
sajaddarabi <sdarabi@nvidia.com>
2025-04-14 17:22:45 -0700

    refractor: None 'ModelType' to doc string for user specific model not defined in framework

--- src/czbenchmarks/datasets/base.py
+++ src/czbenchmarks/datasets/base.py
@@ -86,44 +86,49 @@ class BaseDataset(ABC):
         self._inputs[data_type] = value
 
     def set_output(
-        self, model_type: ModelType, data_type: DataType, value: DataValue
+        self, model_type: ModelType | None, data_type: DataType, value: DataValue
     ) -> None:
-        """Safely set an output with type checking."""
+        """Safely set an output with type checking.
+        Args:
+            model_type (ModelType | None): The type of model associated with the output. 
+                This parameter is used to differentiate between outputs 
+                from various models. It can be set to `None` if the output 
+                is not tied to a specific model type defined in the `ModelType` enum.
+            data_type (DataType): Specifies the data type of the output.
+            value (Any): The value to assign to the output.
+        """
         if data_type.is_input:
             raise ValueError(f"Cannot set input type as output: {data_type.name}")
 
         self._validate_type(value, data_type.dtype, f"Output {data_type.name}")
-
-        if model_type is not None:
-            if model_type not in self._outputs:
-                self._outputs[model_type] = {}
-            self._outputs[model_type][data_type] = value
-        else:
-            if None not in self._outputs:
-                self._outputs[None] = {}
-            self._outputs[None][data_type] = value
+        if model_type not in self._outputs:
+            self._outputs[model_type] = {}
+        self._outputs[model_type][data_type] = value
 
     def get_input(self, data_type: DataType) -> DataValue:
         """Safely get an input with error handling."""
         if data_type not in self._inputs:
             raise KeyError(f"Input {data_type.name} not found")
         return self._inputs[data_type]
-
-    def get_output(self, model_type: ModelType, data_type: DataType) -> DataValue:
-        """Safely get an output with error handling."""
-        if model_type is not None:
-            if model_type not in self._outputs:
-                raise KeyError(f"Outputs for model {model_type.name} not found")
-            if data_type not in self._outputs[model_type]:
-                raise KeyError(
-                    f"Output {data_type.name} not found for model {model_type.name}"
-                )
-            return self._outputs[model_type][data_type]
-        else:
-            for outputs in self._outputs.values():
-                if data_type in outputs:
-                    return outputs[data_type]
-            raise KeyError(f"Output {data_type.name} not found in any model")
+    
+    def get_output(self, model_type: ModelType | None, data_type: DataType) -> DataValue:
+        """Safely get an output with error handling.
+        Args:
+            model_type (ModelType | None): The type of model associated with the output. 
+                This parameter is used to differentiate between outputs 
+                from various models. It can be set to `None` if the output 
+                is not tied to a specific model type defined in the `ModelType` enum.
+            data_type (DataType): Specifies the data type of the output.
+        Returns:
+            DataValue: The value of the output.
+        """
+        if model_type not in self._outputs:
+            raise KeyError(f"Outputs for model {model_type.name} not found")
+        if data_type not in self._outputs[model_type]:
+            raise KeyError(
+                f"Output {data_type.name} not found for model {model_type.name}"
+            )
+        return self._outputs[model_type][data_type]
 
     @abstractmethod
     def _validate(self) -> None:
