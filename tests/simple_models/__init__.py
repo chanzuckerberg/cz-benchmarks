4012ab6438544bde229008de8cceeb7f315c1aad
Oscar <ohvillasenor@gmail.com>
2025-04-28 11:54:38 -0500

    lint

--- tests/simple_models/__init__.py
+++ tests/simple_models/__init__.py
@@ -1,6 +1,2 @@
 """Simple model implementations for testing purposes.
-
-This package contains simplified versions of models that can be used in tests.
-These models don't perform real inference but provide the same interface as
-real models, making them useful for testing the framework's functionality.
-""" 
+"""
--- tests/simple_models/simple_model.py
+++ tests/simple_models/simple_model.py
@@ -15,19 +15,19 @@ class SimpleModel:
         """Generate embeddings on the dataset outputs"""
         # Get the raw data
         adata = dataset.adata.copy()
-        
+
         # Standard preprocessing
         sc.pp.normalize_total(adata, target_sum=1e4)
         sc.pp.log1p(adata)
-        
+
         # Find highly variable genes
         sc.pp.highly_variable_genes(adata, n_top_genes=1000)
         adata = adata[:, adata.var.highly_variable]
-        
+
         # Use PCA as embeddings
         sc.pp.scale(adata)
         sc.tl.pca(adata, n_comps=100)
-        embeddings = adata.obsm['X_pca']
-        
+        embeddings = adata.obsm["X_pca"]
+
         dataset.outputs[self.model_type] = {DataType.EMBEDDING: embeddings}
-        return dataset 
+        return dataset
