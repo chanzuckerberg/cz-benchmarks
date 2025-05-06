878a56d4e1a8c523c1d33a98d94efc3a207cf825
Omar Valenzuela <51972068+valenzuelaomar@users.noreply.github.com>
2025-05-05 17:06:11 -0700

    pr feedback

--- src/czbenchmarks/__init__.py
+++ src/czbenchmarks/__init__.py
@@ -1,3 +1,4 @@
+# Installing this at the root ensures that wherever sklearn is used in this library, it will be GPU accelerated by cuML if it's installed
 def _enable_gpu_acceleration():
     try:
         import cuml.accel
