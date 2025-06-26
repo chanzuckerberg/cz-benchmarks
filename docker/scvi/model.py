29621778c11fe7578aa6dbbe6251ec9b02d4097b
atolopko <atolopko@chanzuckerberg.com>
2025-06-26 18:15:36 -0400

    use scvi model's provided gene alignment mechanism

--- docker/scvi/model.py
+++ docker/scvi/model.py
@@ -65,15 +65,12 @@ class SCVI(SCVIValidator, BaseModelImplementation):
     def run_model(self, dataset: BaseDataset):
         adata = dataset.adata
         batch_keys = self.required_obs_keys
-        adata = filter_adata_by_hvg(
-            adata, f"{self.model_weights_dir}/hvg_names_{dataset.organism.name}.csv.gz"
-        )
         adata.obs["batch"] = functools.reduce(
             lambda a, b: a + b, [adata.obs[c].astype(str) for c in batch_keys]
         )
 
         scvi.model.SCVI.prepare_query_anndata(
-            adata, str(self.model_weights_dir), return_reference_var_names=True
+            adata, str(self.model_weights_dir), return_reference_var_names=False
         )
         vae_q = scvi.model.SCVI.load_query_data(adata, str(self.model_weights_dir))
         vae_q.is_trained = True
