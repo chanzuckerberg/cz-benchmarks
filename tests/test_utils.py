fc24a1a6ebc1b95da57bb9e43eff4ffd11ea2ce4
atolopko <atolopko@chanzuckerberg.com>
2025-07-11 09:48:19 -0400

    eliminate pytest warning

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,7 +5,7 @@ from czbenchmarks.utils import initialize_hydra, import_class_from_config
 
 
 # Sample test class for import testing
-class TestClass:
+class ImportTestClass:
     def __init__(self, param1: str, param2: int):
         self.param1 = param1
         self.param2 = param2
@@ -33,7 +33,7 @@ def test_initialize_hydra():
 def test_import_class_from_config(tmp_path):
     """Test importing a class from a configuration file."""
     # Create a temporary config file
-    config = {"_target_": "tests.test_utils.TestClass", "param1": "test", "param2": 42}
+    config = {"_target_": "tests.test_utils.ImportTestClass", "param1": "test", "param2": 42}
 
     config_path = tmp_path / "test_config.yaml"
     OmegaConf.save(config=config, f=config_path)
@@ -42,7 +42,7 @@ def test_import_class_from_config(tmp_path):
     imported_class = import_class_from_config(str(config_path))
 
     # Verify it's the correct class
-    assert imported_class == TestClass
+    assert imported_class == ImportTestClass
 
     # Test that we can instantiate it with the config parameters
     instance = imported_class(param1="test", param2=42)
