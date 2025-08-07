8fc81408b50cc9253233c50e41e16ec3fca7c972
Michelle Gill <mgill@nvidia.com>
2025-08-06 18:16:26 -0700

    Ready to start testing

--- src/czbenchmarks/datasets/single_cell_perturbation.py
+++ src/czbenchmarks/datasets/single_cell_perturbation.py
@@ -1,10 +1,48 @@
-from typing import Dict, Optional
+from typing import Dict, Optional, List, Tuple
 import io
 from pathlib import Path
 import numpy as np
 import pandas as pd
+import anndata as ad
 from .single_cell import SingleCellDataset
 from .types import Organism
+from ..constants import DEFAULT_SEED
+
+
+def sample_de_genes(
+    de_results: pd.DataFrame,
+    percent_genes_to_mask: float,
+    min_de_genes: int,
+    condition_col: str,
+    gene_col: str,
+    seed: int = DEFAULT_SEED,
+) -> Dict[str, List[str]]:
+    """
+    Sample genes from a differential expression results dataframe.
+
+    Args:
+        de_results (pd.DataFrame): Differential expression results dataframe.
+        percent_genes_to_mask (float): Percentage of genes to mask.
+        min_de_genes (int): Minimum number of differentially expressed genes
+            required to mask that condition. If not met, no genes are masked.
+        condition_col (str): Column name for the condition.
+        gene_col (str): Column name for the gene names.
+        seed (int): Random seed.
+    Returns:
+        Dict[str, List[str]]: Dictionary of target genes and their sampled genes.
+    """
+    np.random.seed(seed)
+    target_genes = de_results[condition_col].unique()
+    target_gene_dict = {}
+    for target in target_genes:
+        gene_names = de_results[de_results[condition_col] == target][gene_col].values
+        n_genes_to_sample = int(len(gene_names) * percent_genes_to_mask)
+        if n_genes_to_sample >= min_de_genes:
+            sampled_genes = np.random.choice(
+                gene_names, size=n_genes_to_sample, replace=False
+            ).tolist()
+            target_gene_dict[target] = sampled_genes
+    return target_gene_dict
 
 
 class SingleCellPerturbationDataset(SingleCellDataset):
@@ -13,30 +51,36 @@ class SingleCellPerturbationDataset(SingleCellDataset):
     perturbed cells.
 
     This class extends `SingleCellDataset` to handle datasets with perturbation
-    data. It includes functionality for validating split values, condition formats,
+    data. It includes functionality for validating condition formats,
     and storing perturbation truth data.
 
     Input data requirements:
     - H5AD file containing single cell gene expression data.
-    - Must have a condition column in `adata.obs` specifying control ("ctrl") and
+    - Must have a column ``{condition_name}`` in `adata.obs` specifying control and
       perturbed conditions.
-    - Must have a split column in `adata.obs` to identify test samples.
     - Condition format must be one of:
-      - ``ctrl`` for control samples.
-      - ``{gene}+ctrl`` for single gene perturbations.
-      - ``{gene1}+{gene2}`` for combinatorial perturbations.
+      - ``{condition_name}`` for control samples.
+      - ``{perturb}`` or ``{perturb}+{condition_name}`` for a single perturbation.
+      - ``{perturb1}+{perturb2}`` for combinatorial perturbations.
 
     Attributes:
-        perturbation_truth (Dict[str, pd.DataFrame]): Control data for each condition.
+        control_cells_ids (dict): Dictionary of control cells IDs for each condition.
+        target_genes_to_save (dict): Dictionary of target genes for each cell.
+        de_results (pd.DataFrame): Differential expression results.
     """
 
-    perturbation_truth: Dict[str, pd.DataFrame]
+    # FIXME MICHELLE add adata?
+    control_cells_ids: dict
+    target_genes_to_save: dict
+    de_results: pd.DataFrame
 
     def __init__(
         self,
         path: Path,
         organism: Organism,
         condition_key: str = "condition",
+        control_name: str = "ctrl",
+        deg_test_name: str = "wilcoxon",
         task_inputs_dir: Optional[Path] = None,
     ):
         """
@@ -47,10 +91,99 @@ class SingleCellPerturbationDataset(SingleCellDataset):
             organism (Organism): Enum value indicating the organism.
             condition_key (str): Key for the column in `adata.obs` specifying conditions.
                 Defaults to "condition".
+            control_name (str): Name of the control condition. Defaults to "ctrl".
+            deg_test_name (str): Name of the differential expression test condition.
+                Options are "wilcoxon" or "t-test". Defaults to "wilcoxon".
             task_inputs_dir (Optional[Path]): Directory for storing task-specific inputs.
         """
         super().__init__("single_cell_perturbation", path, organism, task_inputs_dir)
         self.condition_key = condition_key
+        self.control_name = control_name
+        self.deg_test_name = deg_test_name
+        self.seed = seed
+        # FIXME MICHELLE add parameters for sampling genes?
+
+    def _sample_genes_to_mask(
+        self,
+        de_results: pd.DataFrame,
+        percent_genes_to_mask: float = 0.5,
+        min_de_genes: int = 5,
+        pval_threshold: float = 1e-4,
+        min_logfoldchange: float = 1.0,
+        min_smd: float = 0.55,
+    ) -> Dict[str, List[str]]:
+        # FIXME MICHELLE double check parameter values
+
+        filter = de_results["num_de_genes"] >= min_de_genes
+        filter &= de_results["pval"] < pval_threshold
+
+        if self.deg_test_name == "wilcoxon":
+            filter &= de_results["logfoldchange"] > min_logfoldchange
+        elif self.deg_test_name == "t-test":
+            filter &= de_results["standardized_mean_diff"] > min_smd
+
+        de_results = de_results[filter]
+
+        # TODO update DE results column names so they match the dataset column names
+        target_gene_dict = random_sample_genes(
+            de_results=de_results,
+            percent_genes_to_mask=percent_genes_to_mask,
+            min_de_genes=min_de_genes,
+            condition_col="condition_ensembl_id",
+            gene_col="ensembl_id",
+            seed=self.seed,
+        )
+
+        return target_gene_dict
+
+    def _create_adata(self) -> Tuple[ad.AnnData, dict]:
+        target_gene_dict = self._sample_genes_to_mask()
+        adata = self.adata
+        control_cells_ids = self.control_cells_ids
+
+        # Initialize list to store merged data
+        all_merged_data = []
+
+        # Initialize a dictionary to store target genes for each cell
+        target_genes_to_save = {}
+
+        target_genes = target_gene_dict.keys()
+        for key in target_genes:
+            adata_condition = adata[adata.obs["gene"] == key]
+            adata_control = adata[adata.obs.index.isin(control_cells_ids[key])]
+            adata_condition.obs["condition"] = key
+            adata_control.obs["condition"] = "non-targeting_" + key
+
+            # Check if condition and control data have the same length
+            if len(adata_condition) != len(adata_control):
+                print(
+                    f"Warning: Condition and control data for {key} have different lengths."
+                )
+                continue
+
+            # Merge condition and control data
+            adata_merged = adata_condition.concatenate(adata_control, index_unique=None)
+
+            # Add new column cell_barcode_gene
+            adata_merged.obs["cell_barcode_gene"] = (
+                adata_merged.obs.index.astype(str) + "_" + [key] * len(adata_merged)
+            )
+
+            # Add target genes to the dictionary for each cell
+            for idx in adata_merged.obs["cell_barcode_gene"]:
+                target_genes_to_save[idx] = target_gene_dict[key]
+
+            all_merged_data.append(adata_merged)
+
+        # Combine all adata objects
+        adata_final = all_merged_data[0].concatenate(
+            all_merged_data[1:], index_unique=None
+        )
+
+        # Set the new index
+        adata_final.obs.set_index("cell_barcode_gene", inplace=True)
+
+        return adata_final, target_genes_to_save
 
     def load_data(self) -> None:
         """
@@ -70,44 +203,48 @@ class SingleCellPerturbationDataset(SingleCellDataset):
                 f"Condition key '{self.condition_key}' not found in adata.obs"
             )
 
-        if self.split_key not in self.adata.obs.columns:
-            raise ValueError(f"Split key '{self.split_key}' not found in adata.obs")
+        if not self.adata.obs[self.condition_key].str.contains(self.control_name).any():
+            raise ValueError(
+                f"Condition key '{self.condition_key}' does not contain control condition '{self.control_name}'"
+            )
 
-        # Store control data for each condition in the reference dataset
-        conditions = np.array(list(self.adata.obs[self.condition_key]))
+        if self.deg_test_name not in ["wilcoxon", "t-test"]:
+            raise ValueError(
+                f"Differential expression test name '{self.deg_test_name}' not supported. "
+                "Options are 'wilcoxon' or 't-test'."
+            )
 
-        test_conditions = set(
-            self.adata.obs[self.condition_key][self.adata.obs[self.split_key] == "test"]
+        self.control_cells_ids = self.adata.uns["control_cells_ids"]
+        self.de_results = pd.DataFrame(
+            self.adata.uns["de_results_{self.deg_test_name}"]
         )
 
-        truth_data = {
-            str(condition): pd.DataFrame(
-                data=self.adata[conditions == condition].X.toarray(),
-                index=self.adata[conditions == condition].obs_names,
-                columns=self.adata[conditions == condition].var_names,
-            )
-            for condition in set(test_conditions)
-        }
+        adata_final, target_genes_to_save = self.create_adata()
 
-        self.perturbation_truth = truth_data
-        self.adata = self.adata[self.adata.obs[self.condition_key] == "ctrl"].copy()
+        self.adata = adata_final
+        self.target_genes_to_save = target_genes_to_save
 
     def store_task_inputs(self) -> Path:
         """
-        Store perturbation truth data in JSON files.
+        Store auxiliary data files.
 
-        This method saves the control data for each condition in the
-        `perturbation_truth` attribute to JSON files. The filenames are dynamically
-        generated based on the condition keys.
+        This method saves the IDs of the control cells and the target genes dictonary to JSON files.
 
         Returns:
             Path: Path to the directory storing the task input files.
         """
+        # FIXME MICHELLE how to handle adata?
+        inputs_to_store = {
+            "control_cells_ids": self.control_cells_ids,
+            "target_genes_to_save": self.target_genes_to_save,
+            "de_results": self.de_results,
+        }
         buffer = io.StringIO()
-        for key, df in self.perturbation_truth.items():
+        
+        for key, item in inputs_to_store.items():
             buffer = io.StringIO()
-            df.to_json(buffer)
-            self._store_task_input(f"perturbation_truths/{key}.json", buffer.getvalue())
+            item.to_json(buffer)
+            self._store_task_input(f"{key}.json", buffer.getvalue())
         return self.task_inputs_dir
 
     def _validate(self) -> None:
@@ -116,8 +253,8 @@ class SingleCellPerturbationDataset(SingleCellDataset):
 
         Validates the following:
         - Condition format must be one of:
-          - ``ctrl`` for control samples.
-          - ``{perturb}+ctrl`` for single perturbation.
+          - ``{condition_name}`` for control samples.
+          - ``{perturb}`` or ``{perturb}+{condition_name}`` for single perturbations.
           - ``{perturb1}+{perturb2}`` for combinatorial perturbations.
 
         Raises:
@@ -126,17 +263,27 @@ class SingleCellPerturbationDataset(SingleCellDataset):
         super()._validate()
 
         # Validate condition format
-        conditions = (
-            set(self.adata.obs[self.condition_key]) | self.perturbation_truth.keys()
-        )
+        conditions = set(self.adata.obs[self.condition_key])
 
         for condition in conditions:
-            if condition == "ctrl":
+            if (
+                condition in self.target_genes_to_save.keys()
+            ) | condition == self.control_name:
                 continue
-
-            parts = condition.split("+")
-            if len(parts) != 2:
-                raise ValueError(
+            else:
+                value_error = ValueError(
                     f"Invalid perturbation condition format: {condition}. "
-                    "Must be 'ctrl', '{perturb}+ctrl', or '{perturb1}+{perturb2}'"
+                    f"Must be {self.control_name}, " + "'{perturb}', '{perturb}+"
+                    f"{self.control_name}" + "', or '{perturb1}+{perturb2}'"
                 )
+
+                parts = condition.split("+")
+
+                if len(parts) != 2:
+                    raise value_error
+                else:
+                    for part in parts:
+                        if (part not in self.target_genes_to_save.keys()) & (
+                            part != self.control_name
+                        ):
+                            raise value_error
