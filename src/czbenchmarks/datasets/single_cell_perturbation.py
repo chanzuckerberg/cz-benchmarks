e900c2b1f23a03f00564ff57071760d68c561edf
Polina Binder <pbinder@nvidia.com>
2025-08-11 12:20:56 -0700

    Reset file to match michelle/v1.0-byomodel-replogle_dataset

--- src/czbenchmarks/datasets/single_cell_perturbation.py
+++ src/czbenchmarks/datasets/single_cell_perturbation.py
@@ -8,6 +8,7 @@ import scipy.sparse as sparse
 import pandas as pd
 import anndata as ad
 import logging
+from joblib import Parallel, delayed
 from czbenchmarks.datasets.single_cell import SingleCellDataset
 from czbenchmarks.datasets.types import Organism
 from czbenchmarks.constants import RANDOM_SEED
@@ -63,8 +64,7 @@ class SingleCellPerturbationDataset(SingleCellDataset):
 
     Input data requirements:
     - H5AD file containing single cell gene expression data.
-    - Must have a column ``{condition_name}`` in `adata.obs` specifying control and
-      perturbed conditions.
+    - Must have a column ``{condition_name}`` in `adata.obs` specifying control and perturbed conditions.
     - Condition format must be one of:
 
       - ``{control_name}`` or ``{control_name}_{perturb}`` for control samples.
@@ -74,8 +74,7 @@ class SingleCellPerturbationDataset(SingleCellDataset):
 
     Attributes:
         control_cells_ids (dict): Dictionary of control cells IDs for each condition.
-        de_results (pd.DataFrame): Differential expression results calculated on
-            ground truth data with matched controls.
+        de_results (pd.DataFrame): Differential expression results calculated on ground truth data with matched controls.
         target_genes_to_save (dict): Dictionary of target genes for each cell.
     """
 
