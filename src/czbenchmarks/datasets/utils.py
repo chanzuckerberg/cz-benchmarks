2a1e1669a0fb910f0ed86a16c06a104d05404aa7
Michelle Gill <mgill@nvidia.com>
2025-08-20 08:38:59 -0700

    ruff

--- src/czbenchmarks/datasets/utils.py
+++ src/czbenchmarks/datasets/utils.py
@@ -157,7 +157,9 @@ def run_multicondition_dge_analysis(
         )
 
     if return_merged_adata:
-        log.warning("return_merged_adata is True, which can consume a large amount of memory.")
+        log.warning(
+            "return_merged_adata is True, which can consume a large amount of memory."
+        )
 
     obs = adata.obs
     obs_index = obs.index
