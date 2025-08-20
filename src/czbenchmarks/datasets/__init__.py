4e00e616c9986007dca665e091684331bf8aab9b
Saurabh Gupta <saurabh.gupta@contractor.chanzuckerberg.com>
2025-08-20 13:07:50 -0700

    support for using local dataset

--- src/czbenchmarks/datasets/__init__.py
+++ src/czbenchmarks/datasets/__init__.py
@@ -1,4 +1,4 @@
-from .utils import load_dataset, list_available_datasets
+from .utils import load_dataset, load_local_dataset, list_available_datasets
 from .single_cell_labeled import SingleCellLabeledDataset
 from .single_cell_perturbation import SingleCellPerturbationDataset
 from .dataset import Dataset
@@ -6,6 +6,7 @@ from .types import Organism
 
 __all__ = [
     "load_dataset",
+    "load_local_dataset",
     "list_available_datasets",
     "SingleCellLabeledDataset",
     "SingleCellPerturbationDataset",
