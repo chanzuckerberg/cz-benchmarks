3c539e1593afe1620da3866d01f769e0577f0d53
Polina Binder <pbinder@nvidia.com>
2025-09-23 13:59:42 -0700

    reverting a change

--- src/czbenchmarks/datasets/utils_single_cell.py
+++ src/czbenchmarks/datasets/utils_single_cell.py
@@ -2,35 +2,8 @@ from typing import Tuple
 import logging
 import anndata as ad
 import numpy as np
-import numba as nb
 
 logger = logging.getLogger(__name__)
-
-
-@nb.njit(parallel=True, fastmath=True)
-def colwise_nonzero_mean_var_numba(X):
-    n_rows, n_cols = X.shape
-    sums = np.zeros(n_cols, np.float64)
-    sumsq = np.zeros(n_cols, np.float64)
-    counts = np.zeros(n_cols, np.int64)
-
-    for i in range(n_rows):
-        for j in range(n_cols):
-            v = X[i, j]
-            if v != 0.0:
-                sums[j] += v
-                sumsq[j] += v * v
-                counts[j] += 1
-
-    mean = np.zeros(n_cols, np.float64)
-    var = np.zeros(n_cols, np.float64)
-    for j in nb.prange(n_cols):
-        c = counts[j]
-        if c > 0:
-            mu = sums[j] / c
-            mean[j] = mu
-            var[j] = sumsq[j] / c - mu * mu
-    return mean, var
 
 
 def create_adata_for_condition(
@@ -71,6 +44,7 @@ def create_adata_for_condition(
     label_cond = [condition] * len(adata_condition)
     label_ctrl = [f"{control_name}_{condition}"] * len(adata_control)
     adata_merged.obs[condition_key] = label_cond + label_ctrl
+
     # Add condition to cell_barcode_gene column and set as index
     adata_merged.obs_names = adata_merged.obs_names.astype(str) + "_" + condition
 
