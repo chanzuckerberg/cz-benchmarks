364aa0685acd7184ad89bf148d8f8e2d5d4cd83c
Michelle Gill <mgill@nvidia.com>
2025-09-25 12:02:43 -0700

    WIP files

--- dge_files/controlcells_run.py
+++ dge_files/controlcells_run.py
@@ -16,28 +16,32 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    filtered_data_path = f"{os.environ['HOME']}/.cz-benchmarks/datasets/replogle_k562_essential_perturbpredict_de_results_control_cells.h5ad"
+    # filtered_data_path = f"{os.environ['HOME']}/.cz-benchmarks/datasets/replogle_k562_essential_perturbpredict_de_results_control_cells.h5ad"
+    filtered_data_path = "/data2/czbenchmarks/replogle2022/K562_essential_raw_singlecell_01.h5ad"
     adata_filtered = ad.read_h5ad(filtered_data_path, backed=None)
+    adata_filtered.obs = adata_filtered.obs.rename(columns={'gene_id':'condition', 'gene':'condition_name'})
+    adata_filtered.var = adata_filtered.var.rename(columns={'gene_name':'gene'})
     adata_filtered.obs['condition'] = adata_filtered.obs['condition'].astype(str)
-    control_cells_ids_input = adata_filtered.uns["control_cells_ids"]
 
-    cr4_data_path = (
-        "/data2/czbenchmarks/replogle2022/raw_h5ad_from_cr4/K562_essential_mtx.h5ad"
-    )
-    adata_cr4 = ad.read_h5ad(cr4_data_path, backed=None)
-    adata_cr4.var.index.name = "gene_id"
-    adata_cr4.var.rename(columns={"gene_name": "gene"}, inplace=True)
-    adata_cr4.obs.rename(columns={"gene_id": "condition"}, inplace=True)
+
+    # cr4_data_path = (
+    #     "/data2/czbenchmarks/replogle2022/raw_h5ad_from_cr4/K562_essential_mtx.h5ad"
+    # )
+    # adata_cr4 = ad.read_h5ad(cr4_data_path, backed=None)
+    # adata_cr4.var.index.name = "gene_id"
+    # adata_cr4.var.rename(columns={"gene_name": "gene"}, inplace=True)
+    # adata_cr4.obs.rename(columns={"gene_id": "condition"}, inplace=True)
 
     # num_conditions = 10
+    # control_cells_ids_input = adata_filtered.uns["control_cells_ids"]
     # condition_list = np.asarray([x for x in control_cells_ids_input.keys() if x != 'non-targeting'])
-    condition_list = np.asarray([x for x in adata_filtered.obs.condition.unique() if x != 'non-targeting'])
     # condition_list = np.random.choice(
     #     condition_list, size=num_conditions, replace=False
     # )
     # condition_list = list(condition_list)
     # new_control_cells_ids = {k: control_cells_ids_input[k] for k in condition_list}
     # control_cells_ids = new_control_cells_ids
+    condition_list = np.asarray([x for x in adata_filtered.obs.condition.unique() if x != 'non-targeting'])
 
     control_cells_ids_new = {}
     total_conditions = len(condition_list)
@@ -47,7 +51,7 @@ if __name__ == "__main__":
         for condition in condition_list:
             print(f"Getting matched controls for {condition}")
             pert_cells, matched_controls = get_matched_controls(
-                adata=adata_cr4,
+                adata=adata_filtered,
                 perturbation=condition,
                 min_cells=1,
                 matchtype="GEM",
@@ -67,7 +71,7 @@ if __name__ == "__main__":
             pbar.update(1)
 
     print("Saving new control cells ids to json")
-    with open("/data2/czbenchmarks/control_cells_ids_replogle_k562_essential_perturbpredict_validate.json", "w") as f:
+    with open("/data2/czbenchmarks/control_cells_ids_replogle_k562_essential_perturbpredict_new.json", "w") as f:
         json.dump(control_cells_ids_new, f)
 
 
