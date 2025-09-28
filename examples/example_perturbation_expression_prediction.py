234d8f8a0f4604d5c9cb43f1a12118fe8555789f
Michelle Gill <mgill@nvidia.com>
2025-09-28 06:48:06 -0700

    Improvements to example script

--- examples/example_perturbation_expression_prediction.py
+++ examples/example_perturbation_expression_prediction.py
@@ -1,7 +1,12 @@
 import logging
 import sys
 import argparse
-from czbenchmarks.datasets import load_dataset
+from typing import Optional
+import omegaconf
+import hydra
+from hydra.utils import instantiate
+import numpy as np
+
 from czbenchmarks.tasks.single_cell import (
     PerturbationExpressionPredictionTask,
     PerturbationExpressionPredictionTaskInput,
@@ -10,51 +15,25 @@ from czbenchmarks.tasks.single_cell.perturbation_expression_prediction import (
     load_perturbation_task_input_from_saved_files,
 )
 from czbenchmarks.tasks.utils import print_metrics_summary
-import numpy as np
 from czbenchmarks.datasets import SingleCellPerturbationDataset
 from czbenchmarks.tasks.types import CellRepresentation
-import tempfile
-import yaml
-from pathlib import Path
 
-if __name__ == "__main__":
-    """Runs a task to calculate perturbation metrics. 
+from czbenchmarks.utils import initialize_hydra
+from czbenchmarks.file_utils import download_file_from_remote
 
-    As input, this uses a SingleCellPerturbationDataset. Currently, this assumes 
-    data from the Replogle et al. 2022 dataset. Addtionally, this contains 
-    differentially expressed genes for each perturbation. The extent of the 
-    perturbation is merged with the willcoxon test or t-test.
-    
-    The dataset is filtered based on the type of statistical test, along with the minimum 
-    number of differentially expressed genes, maximum p-value, and the minimum 
-    log fold change or standardized mean difference. During the dataset generation, 
-    the specified percentage of genes to mask is randomly selected.
-    
-    The dataset can be saved after filtering, and then loaded back in.
-    
-    In this example, a random model output is used. Instead, any model output of
-    the same shape as the dataset's adata can be used.
-    
-    The task computes the log fold change in model predicted expression of 
-    differentially expressed genes between perturbed and non-targeting groups.
-    It then calculates the correlation between ground truth and predicted log 
-    fold change for each condition using a variety of metrics.    
-    """
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
-    # Parse command line arguments
+
+def parse_args():
+    """Parse command line arguments."""
     parser = argparse.ArgumentParser(
         description="Run perturbation expression prediction task"
     )
     parser.add_argument(
-        "--save-inputs",
+        "--save-task-inputs",
         action="store_true",
-        help="Save dataset task inputs to disk and load them back (demonstrates save/load functionality)",
-    )
-    parser.add_argument(
-        "--metric",
-        type=str,
-        default="wilcoxon",  # Set this to correspond to the type of statistical test used to determine differentially expressed genes
-        help="Metric to use for DE analysis",
+        help="Save dataset task inputs to disk and load them back "
+        "(demonstrates save/load functionality)",
     )
     parser.add_argument(
         "--percent_genes_to_mask",
@@ -66,7 +45,7 @@ if __name__ == "__main__":
         "--min_logfoldchange",
         type=float,
         default=1.0,
-        help="Minimum absolute log-fold change for DE filtering (used when --metric=wilcoxon)",
+        help="Minimum absolute log-fold change for DE filtering",
     )
     parser.add_argument(
         "--pval_threshold",
@@ -80,48 +59,88 @@ if __name__ == "__main__":
         default=5,
         help="Minimum number of DE genes required to mask a condition",
     )
-    parser.add_argument(
-        "--min_smd",
-        type=float,
-        default=0.55,
-        help="Minimum standardized mean difference for DE filtering (used when --metric=t-test)",
-    )
+    return parser.parse_args()
 
-    args = parser.parse_args()
 
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+def load_dataset_config(
+    dataset_name: str,
+    config_name: str = "datasets",
+    dataset_update_dict: Optional[dict] = None,
+):
+    """Customize dataset class instantiation parameters using cli args
 
-    # Instantiate a config and load the input data
-    cfg = {
-        "datasets": {
-            "replogle_k562_essential_perturbpredict": {
-                "percent_genes_to_mask": args.percent_genes_to_mask,
-                "deg_test_name": args.metric,
-                "min_logfoldchange": args.min_logfoldchange,
-                "pval_threshold": args.pval_threshold,
-                "min_de_genes_to_mask": args.min_de_genes_to_mask,
-                "min_smd": args.min_smd,
-            }
-        }
+    Args:
+        dataset_name: Name of the dataset to load
+        dataset_update_dict: Optional dictionary of dataset parameters to update
+
+    Returns:
+        Dataset configuration
+    """
+    initialize_hydra()
+    cfg = hydra.compose(config_name=config_name)
+    dataset_cfg = cfg.datasets[dataset_name]
+    if dataset_update_dict:
+        with omegaconf.open_dict(dataset_cfg) as d:
+            d.update(dataset_update_dict)
+
+    return dataset_cfg
+
+
+if __name__ == "__main__":
+    """Runs a task to calculate perturbation metrics. 
+
+    As input, this uses a SingleCellPerturbationDataset with data from the
+    Replogle et al. 2022 dataset. Additionally, this dataset contains 
+    control-matched differentially expressed genes for each perturbation. 
+    The extent of the perturbation is measured with the Wilcoxon rank-sum test.
+    
+    The dataset is filtered based on statistical parameters, along with the minimum 
+    number of differentially expressed genes, and then treatment cells are
+    matched with control cells. The differentially expressed genes are used in
+    the selection of genes for masking. 
+    
+    There is an optinal flag, --save-inputs, to save the outputs of the dataset
+    for loading into the task at a future time.
+    
+    In this example, a random model output is used. Instead, any model output of
+    the same shape as the dataset's adata can be used.
+    
+    The task uses the pre-computed log fold change in model predicted expression of 
+    genes between perturbed and control groups. It then calculates the correlation 
+    between ground truth and predicted log fold change for each condition.
+    """
+
+    args = parse_args()
+    dataset_name = "replogle_k562_essential_perturbpredict"
+    logging.info(f"Loading dataset for {dataset_name} with args: {args}")
+
+    dataset_update_dict = {
+        "percent_genes_to_mask": args.percent_genes_to_mask,
+        "min_logfoldchange": args.min_logfoldchange,
+        "pval_threshold": args.pval_threshold,
+        "min_de_genes_to_mask": args.min_de_genes_to_mask,
     }
-
-    with tempfile.TemporaryDirectory() as d:
-        cfg_path = Path(d) / "config.yaml"
-        cfg_path.write_text(yaml.safe_dump(cfg))
-        dataset: SingleCellPerturbationDataset = load_dataset(
-            "replogle_k562_essential_perturbpredict", config_path=str(cfg_path)
-        )  # TODO: Once PR 381 is merged, use the new load_local_dataset function
+    # TODO could be used to improve flexibility of current load_dataset function
+    dataset_cfg = load_dataset_config(
+        dataset_name=dataset_name, dataset_update_dict=dataset_update_dict
+    )
+    dataset_cfg["path"] = download_file_from_remote(dataset_cfg["path"])
+    dataset: SingleCellPerturbationDataset = instantiate(dataset_cfg)
+    dataset.load_data()
 
     # Choose approach based on flag
-    if args.save_inputs:
-        print("Using save/load approach...")
-        # Save and load dataset task inputs
+    if args.save_task_inputs:
+        logging.info("Using save/load approach...")
+
+        # Save and load dataset task inputs -- useful for caching dataset output
         task_inputs_dir = dataset.store_task_inputs()
-        print(f"Task inputs saved to: {task_inputs_dir}")
+        logging.info(f"Task inputs saved to: {task_inputs_dir}")
+
         task_input = load_perturbation_task_input_from_saved_files(task_inputs_dir)
-        print("Task inputs loaded from saved files")
+        logging.info("Task inputs loaded from saved files")
     else:
-        print("Creating task input directly from dataset...")
+        logging.info("Creating task input directly from dataset...")
+
         # Create task input directly from dataset
         task_input = PerturbationExpressionPredictionTaskInput(
             de_results=dataset.de_results,
@@ -137,6 +156,8 @@ if __name__ == "__main__":
     )
 
     # Run task
-    task = PerturbationExpressionPredictionTask(metric=args.metric)
+    task = PerturbationExpressionPredictionTask(
+        control_prefix=dataset_cfg["control_name"]
+    )
     metrics_dict = task.run(model_output, task_input)
     print_metrics_summary(metrics_dict)
