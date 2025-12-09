58ef1c6fd14b94c03462d4fd9dd4b981b5355672
Xinmei Gui <xgui@chanzuckerberg.com>
2025-12-09 13:21:28 -0800

    typo fix

--- tests/test_dataset_task_e2e_regression.py
+++ tests/test_dataset_task_e2e_regression.py
@@ -167,7 +167,7 @@ def test_clustering_task_regression(dataset):
     # Regression test: Compare against expected results
     # Expected results (captured from CZI Virtual Cells Platform benchmarking results at s3://cz-benchmarks-results-dev/v0.10.0/results/20250529_004446-f1736d11.json)
     # If this test fails, update expected_metrics with new values from a successful run AFTER a computational biologist has validated the new results.
-    # The tolerance of 0.01 may fail the test transiently, due to non-determinisium in the clustering operations. 
+    # The tolerance of 0.01 may fail the test transiently, due to non-determinism in the clustering operations. 
     expected_metrics = [
         {"metric_type": "adjusted_rand_index", "value": 0.39411565248721414},
         {"metric_type": "normalized_mutual_info", "value": 0.6947935391845789},
